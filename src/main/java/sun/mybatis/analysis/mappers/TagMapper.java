59e08365001334c0c945dcab503b50515dd65abd
胡枫 <yameng.sunyameng@koubei.com>
2019-05-30 23:41:44 +0800

    tag mapper

---
+++ src/main/java/sun/mybatis/analysis/mappers/TagMapper.java
@@ -0,0 +1,16 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package sun.mybatis.analysis.mappers;
+
+import sun.mybatis.analysis.domain.blog.Tag;
+import sun.mybatis.analysis.entity.Student;
+
+/**
+ * @author hufeng
+ * @version 1.0: TagMapper.java, v 0.1 2019年05月30日 23:37 hufeng Exp $
+ */
+public interface TagMapper {
+    int insert(Tag tag);
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,30 +1,63 @@
-# drop table if exists student;
-create table if not exists student
+CREATE TABLE IF NOT EXISTS author
 (
-  id         int not null primary key auto_increment,
-  name       varchar(20),
-  branch     varchar(20),
-  percentage int(2),
-  phone      varchar(30),
-  email      varchar(30)
+    id                INT          NOT NULL AUTO_INCREMENT,
+    username          VARCHAR(255) NOT NULL,
+    password          VARCHAR(255) NOT NULL,
+    email             VARCHAR(255) NOT NULL,
+    bio               BLOB,
+    favourite_section VARCHAR(25),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS blog
+(
+    id        INT NOT NULL AUTO_INCREMENT,
+    author_id INT NOT NULL,
+    title     VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS post
+(
+    id         INT          NOT NULL AUTO_INCREMENT,
+    blog_id    INT,
+    author_id  INT          NOT NULL,
+    created_on TIMESTAMP    NOT NULL,
+    section    VARCHAR(25)  NOT NULL,
+    subject    VARCHAR(255) NOT NULL,
+    body       LONGBLOB     NOT NULL,
+    draft      INT          NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (blog_id) REFERENCES blog (id)
 );
 
 
-create table if not exists author
+CREATE TABLE IF NOT EXISTS tag
 (
-  id       int not null primary key auto_increment,
-  username varchar(20),
-  password varchar(20),
-  bio      varchar(20),
-  email    varchar(20)
+    id   INT          NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-
-create table if not exists blog
+CREATE TABLE IF NOT EXISTS post_tag
 (
-  id          int not null primary key auto_increment,
-  title       varchar(20),
-  body        varchar(200),
-  author_id   int not null,
-  create_time datetime
+    post_id INT NOT NULL,
+    tag_id  INT NOT NULL,
+    PRIMARY KEY (post_id, tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS comment
+(
+    id      INT          NOT NULL AUTO_INCREMENT,
+    post_id INT          NOT NULL,
+    name    VARCHAR(255) NOT NULL,
+    comment LONGTEXT     NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS node
+(
+    id        INT NOT NULL,
+    parent_id INT,
+    PRIMARY KEY (id)
 );
